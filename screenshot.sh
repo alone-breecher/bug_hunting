fae49b82c05e262c71a4095ceac6fd5c70942a95
Anzil <76893049+alone-breecher@users.noreply.github.com>
2021-08-22 13:00:11 +0530

    Update screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -2,4 +2,14 @@
 
 read -p "enter the path to sub domains: " domains
 
-python3 ~/Desktop/bug_finders/EyeWitness/Python/EyeWitness.py -f $domains --web
+if [ -f /usr/local/bin/Eyewitness/python/Eyewitness.py ] 
+then
+ echo "${magenta} [+] Already installed Eyewitness ${reset}"
+else
+ echo "${blue} [+] Installing Eyewitness ${reset}"
+ cd /usr/local/bin
+ git clone https://github.com/FortyNorthSecurity/EyeWitness
+fi
+echo " "
+
+python3 /usr/local/bin/EyeWitness/Python/EyeWitness.py -f $domains --web
