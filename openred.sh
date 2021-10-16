728614b204a5d4b0114a55d6aba34899c51fb5c0
Anzil <76893049+alone-breecher@users.noreply.github.com>
2021-10-16 04:53:04 -0700

    Add files via upload

---
+++ openred.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+ 
+#colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+reset=`tput sgr0`
+ 
+read -p "Enter domain name : " DOM
+ 
+if [ -d ~/Desktop/ ]
+then
+  echo " "
+else
+  mkdir ~/Desktop
+fi
+ 
+
+
+if [ -d ~/Desktop/$DOM/openredirects_Discovery ]
+then
+  echo " "
+else
+  mkdir ~/Desktop/$DOM/openredirects_Discovery
+ 
+fi
+ 
+ 
+echo "${blue} [+] Started openredirects Discovery Scanning ${reset}"
+echo " "
+
+#wordlist
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+if [ -f ~/Desktop/openredirects.txt ]
+then
+ echo " "
+else
+ echo "${blue} [+] Downloading wordlists ${reset}"
+ wget https://raw.githubuseropenredirects.com/v0re/dirb/master/wordlists/common.txt -P ~/Desktop/tools/
+fi
+
+#feroxbuster
+if [ -f /usr/bin/feroxbuster ]
+then
+ echo "${magenta} [+] Running Feroxbuster for openredirects discovery${reset}"
+ for url in $(cat ~/Desktop/$DOM/Subdomains/all-alive-subs.txt);do
+ reg=$(echo $url | sed -e 's;https\?://;;' | sed -e 's;/.*$;;')
+ feroxbuster --url $url -w ~/Desktop/openredirects.txt --depth 3 --threads 300 --output ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt
+done
+else
+ echo "${blue} [+] Installing Feroxbuster ${reset}"
+ wget https://github.com/epi052/feroxbuster/releases/download/v1.5.2/x86_64-linux-feroxbuster.zip -P ~/Desktop/tools/feroxbuster
+ unzip ~/Desktop/tools/feroxbuster/x86_64-linux-feroxbuster.zip -d ~/go/bin/
+ chmod 777 ~/go/bin/feroxbuster
+ echo "${magenta} [+] Running Feroxbuster for openredirects discovery${reset}"
+ for url in $(cat ~/Desktop/$DOM/Subdomains/all-alive-subs.txt);do
+ reg=$(echo $url | sed -e 's;https\?://;;' | sed -e 's;/.*$;;')
+ feroxbuster --url $url -w ~/Desktop/openredirects.txt --depth 3 --threads 300 --output ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt
+done
+fi
+
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+echo "${blue} [+] Succesfully saved as openredirects_discovery_result.txt ${reset}"
+echo " "
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+echo "${magenta} [+] Sorting According to Status Codes ${reset}"
+cat ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt | grep 200 | awk '{print $2}' > ~/Desktop/$DOM/openredirects_Discovery/status_code_200.txt  
+cat ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt | grep 204 | awk '{print $2}' > ~/Desktop/$DOM/openredirects_Discovery/status_code_204.txt
+cat ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt | grep 301 | awk '{print $2}' > ~/Desktop/$DOM/openredirects_Discovery/status_code_301.txt
+cat ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt | grep 302 | awk '{print $2}' > ~/Desktop/$DOM/openredirects_Discovery/status_code_302.txt
+cat ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt | grep 307 | awk '{print $2}' > ~/Desktop/$DOM/openredirects_Discovery/status_code_307.txt
+cat ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt | grep 308 | awk '{print $2}' > ~/Desktop/$DOM/openredirects_Discovery/status_code_308.txt
+cat ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt | grep 401 | awk '{print $2}' > ~/Desktop/$DOM/openredirects_Discovery/status_code_401.txt
+cat ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt | grep 403 | awk '{print $2}' > ~/Desktop/$DOM/openredirects_Discovery/status_code_403.txt
+cat ~/Desktop/$DOM/openredirects_Discovery/openredirects_discovery_result.txt | grep 405 | awk '{print $2}' > ~/Desktop/$DOM/openredirects_Discovery/status_code_405.txt
+echo " "
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+echo "${blue} [+] Succesfully saved the results according to their status codes ${reset}"
+echo " "
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+echo " "
+echo "${red} [+] Thank you for using R3C0Nizer${reset}"
+echo ""
+echo "${yellow} ---------------------------------- xxxxxxxx ---------------------------------- ${reset}"
+
